85ad391a52b32105db65f028589c2a072a690728
sachinam1397 <sachinam1397@gmail.com>
2020-07-12 22:43:00 +0530

    contestHandler

--- Excel reader/contestHandler.py
+++ Excel reader/contestHandler.py
@@ -6,9 +6,14 @@ import xlrd
 
 class ContestHandler():
 	def __init__(self, fileName):
-		self.all_records = 
-		pass
-		# Read file and store it
+		self.all_records = xlrd.open_workbook(fileName)
+		self.sheet = self.all_records.sheet_by_index(0)
+		self.user = {}
+		for i in range(1,self.sheet.nrows):
+			self.temp_dict = {}
+			self.temp_dict["Name"] = self.sheet.cell_value(i,0)
+			self.temp_dict["Email"] = self.sheet.cell_value(i,1)
+			self.temp_dict["Problems"]
 
 
 	def getData(self, username = None):
@@ -17,3 +22,10 @@ class ContestHandler():
 
 	def getUsernameList(self):
 		pass
+
+
+if __name__ == '__main__':
+	contestData = ContestHandler("./AlgorithmsP1-result-837a223.xlsx")
+	for i in range(contestData.sheet.ncols):
+		print(contestData.sheet.cell_value(0,i))
+	
