1903f3014687dc0b19c2af3a459b68fd2e407107
sachinam1397 <sachinam1397@gmail.com>
2020-07-14 23:42:03 +0530

    Main File

--- Merge Data/main.py
+++ Merge Data/main.py
@@ -7,9 +7,9 @@ from dataPostProcessHandler import PostProcessHandler
 
 def main():
 	# Read all 3 files and generate their handlers
-	plagObject = PlagiarismHandler('AlgorithmsP1-plagiarism-report.xlsx')
-	enrolmentObject = EnrolmentHandler('Algorithm_P1.xlsx')    # Also, add these files into git repo
-	contestDataObject = ContestHandler('AlgorithmsP1-result-837a223.xlsx')
+	plagObject = PlagiarismHandler('AlgorithmsP1-plagiarism-final.xlsx')
+	enrolmentObject = EnrolmentHandler('Algo P1 -final.xlsx')    # Also, add these files into git repo
+	contestDataObject = ContestHandler('AlgorithmsP1-result-final.xlsx')
 
 	# Process 3 files to create reports
 	dataHandlerObject = ProcessData(plagObject, enrolmentObject, contestDataObject)
