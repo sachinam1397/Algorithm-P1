003826b588e0565d7655011551f9c84efc2a5330
valiant1011 <prakharp789@gmail.com>
2020-07-14 23:03:40 +0530

    Fixed multiple contest handling in contest reader

--- Excel reader/contestHandler.py
+++ Excel reader/contestHandler.py
@@ -9,14 +9,30 @@ class ContestHandler():
 		self.all_records = xlrd.open_workbook(fileName)
 		self.sheet = self.all_records.sheet_by_index(0)
 		self.user = {}
-		for i in range(1,self.sheet.nrows):
+		for i in range(1, self.sheet.nrows):
 			self.temp_dict = {}
 			self.temp_dict["Name"] = self.sheet.cell_value(i,0)
 			self.temp_dict["Email"] = self.sheet.cell_value(i,1)
 			self.temp_dict["Problems Solved"] = ((int(self.sheet.cell_value(i,11))//4) + (int(self.sheet.cell_value(i,12))//4))
 			self.temp_dict["Tab Switches"] = self.sheet.cell_value(i,7)
+
 			username = self.sheet.cell_value(i,9).split('@')
-			self.user[username[1].lower()] = self.temp_dict
+			username = username[1].lower()
+			# --------------------------------------------------------------------
+			# New changes here:
+			self.contestID = self.sheet.cell_value(i,    x      )
+
+			if self.contestID == 'B':
+				# Overwrite any data for contest A, as contest B has more priority
+				self.user[username] = self.temp_dict
+
+			elif self.contestID == 'A':
+				# Check if any record of contest B for this user has already been read:
+				data_ = self.user.get(username, None)
+				# If no previous record is found:
+				if data_ == None:
+					# User has not given contest B, so accept this contest data
+					self.user[username] = self.temp_dict
 
 
 	def getData(self, username = None):
