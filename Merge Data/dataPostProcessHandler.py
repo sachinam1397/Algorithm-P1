fab601b3cf59c0c1ccae5abe9afcde3c55436d0b
valiant1011 <prakharp789@gmail.com>
2020-07-14 23:39:26 +0530

    Updated post processing'

--- Merge Data/dataPostProcessHandler.py
+++ Merge Data/dataPostProcessHandler.py
@@ -1,18 +1,45 @@
 from openpyxl import Workbook
+# Batch enrolments:
+# B1 1-32
+# B2 33-64
+# B3 65-96
+# B4 97-129 , 264,269
+# B5 130-160, 270
+# B6 161-192, 265-268
+# B7 178, 193-224
+# B8 226-263 
 
 class PostProcessHandler():
 	def __init__(self, dataHandlerObject):
+		# List of all batches
 		self.batchList = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'Unknown']
+		# This dict holds the batch data as list of tuples(enrolment, score)
+		self.batchDict = {'B1' : [], 'B2' : [], 'B3' : [], 'B4' : [], 'B5' : [], 'B6' : [], 'B7' : [], 'B8' : [], 'Unknown' : []}
+		
 		self.dataHandlerObject = dataHandlerObject
 
+		self.makeDict()
 		self.makeSheets()
+
+
+	def makeDict(self):
+		for enrolment in range(1, 271):
+			enrolmentString = "171B" + "{0:0=3d}".format(enrolment)
+
+			userData = self.dataHandlerObject.getUserData(enrolmentString)
+			userBatch = self.getBatch(enrolmentString)
+
+			if userData == None:
+				userData = ('<< Unknown >>', enrolmentString, 4, 0, '-')
+
+			self.batchDict[userBatch].append(userData)
 
 
 	def makeSheets(self):
 		for batch in self.batchList:
-			batchData = self.dataHandlerObject.getBatchReport(batch)
+			batchData = self.batchDict[batch]
+
 			fileName = batch + '.xlsx'
-			
 			workbook = Workbook()
 			activeSheet = workbook.active
 			activeSheet.title = batch + ' Marks'
@@ -23,3 +50,29 @@ class PostProcessHandler():
 				activeSheet.append(user)
 
 			workbook.save(fileName)
+
+
+	def getBatch(self, enrolment):
+		# enrolment is in the form 181Bxxx, eDigit = xxx
+		try:
+			if type(enrolment) == type(""):
+				eDigit = int(enrolment[4:])	# Remove 181B
+		except:
+			return "Unknown"
+
+		if eDigit >= 1 and eDigit <= 32:
+			return 'B1'
+		elif eDigit >= 33 and eDigit <= 64:
+			return 'B2'
+		elif eDigit >= 65 and eDigit <= 96:
+			return 'B3'
+		elif (eDigit >= 193 and eDigit <= 224) or eDigit == 178:
+			return 'B7'
+		elif eDigit >= 226 and eDigit <= 263:
+			return 'B8'
+		elif (eDigit >= 97 and eDigit <= 129) or eDigit == 264 or eDigit == 269:
+			return 'B4'
+		elif (eDigit >= 130 and eDigit <= 160) or eDigit == 270:
+			return 'B5'
+		elif (eDigit >= 161 and eDigit <= 192) or eDigit in [265, 266, 267, 268]:
+			return 'B6'
--- Merge Data/processData.py
+++ Merge Data/processData.py
@@ -1,21 +1,10 @@
-# Batch enrolments:
-# B1 1-32
-# B2 33-64
-# B3 65-96
-# B4 97-129 , 264,269
-# B5 130-160, 270
-# B6 161-192, 265-268
-# B7 178, 193-224
-# B8 226-263
-
 class ProcessData():
 	def __init__(self, plagObject, enrolmentObject, contestDataObject):
 		self.plagObject = plagObject
 		self.enrolmentObject = enrolmentObject
 		self.contestDataObject = contestDataObject
 
-		# This dict holds the batch data as list of tuples(enrolment, score)
-		self.batchDict = {'B1' : [], 'B2' : [], 'B3' : [], 'B4' : [], 'B5' : [], 'B6' : [], 'B7' : [], 'B8' : [], 'Unknown' : []}
+		self.allDict = {}
 		# Get the list of user names from contest class
 		self.userList = self.contestDataObject.getUserNameList()		
 		# Make results
@@ -31,72 +20,50 @@ class ProcessData():
 			userTabSwitches = userData["Tab Switches"]
 			userPlagiarismStatus = self.plagObject.getStatus(user)
 
-
 			userEnrolment = self.enrolmentObject.getEnrolment(user)
-			# print(userEnrolment)
 			if userEnrolment == None:
-				# Bad google form response
-				userEnrolment = 'Incorrect Form data filled.'
-				userBatch = 'Unknown'
+				# Fuck you user :P
+				continue
+
+			userContestID = userData.get("Contest ID", 'A')		# Contest A or B
+			if userContestID == 'A':
+				fullMarks = 8
+				halfMarks = 7
+				noneMarks = 6
 			else:
-				userBatch = self.getBatch(userEnrolment)
+				fullMarks = 6
+				halfMarks = 5
+				noneMarks = 4
+			discMarks = 3
 
 			# Check user plagiarism status
 			if userPlagiarismStatus == True:
-				score = 3
+				score = discMarks
 				userPlagiarismStatus = 'Disqualified due to Plagiarism'
 				if userTabSwitches > 5:
 					userProblemSolved = 'Also disqualified due to Tab switches'
 
 			elif userTabSwitches > 5:
-				score = 3
+				score = discMarks
 				userProblemSolved = 'Disqualified due to Tab switches'
 				userPlagiarismStatus = '-'
 
 			elif userProblemSolved == 0:
-				score = 6
+				score = noneMarks
 				userPlagiarismStatus = '-'
 
 			elif userProblemSolved == 1: 
-				score = 7
+				score = halfMarks
 				userPlagiarismStatus = '-'
 
 			elif userProblemSolved == 2:
-				score = 8
+				score = fullMarks
 				userPlagiarismStatus = '-'
 
-			# Make data in required format
-			data = (user, userEnrolment, score, userProblemSolved, userPlagiarismStatus)
+			# store data
+			data = (user, userEnrolment.lower(), score, userProblemSolved, userPlagiarismStatus)
+			self.allDict[userEnrolment.lower()] = data
 
-			# Store data
-			self.batchDict[userBatch].append(data)
-
-			
-	def getBatch(self, enrolment):
-		# enrolment is in the form 181Bxxx, eDigit = xxx
-		try:
-			if type(enrolment) == type(""):
-				eDigit = int(enrolment[4:])	# Remove 181B
-		except:
-			return "Unknown"
-
-		if eDigit >= 1 and eDigit <= 32:
-			return 'B1'
-		elif eDigit >= 33 and eDigit <= 64:
-			return 'B2'
-		elif eDigit >= 65 and eDigit <= 96:
-			return 'B3'
-		elif (eDigit >= 193 and eDigit <= 224) or eDigit == 178:
-			return 'B7'
-		elif eDigit >= 226 and eDigit <= 263:
-			return 'B8'
-		elif (eDigit >= 97 and eDigit <= 129) or eDigit == 264 or eDigit == 269:
-			return 'B4'
-		elif (eDigit >= 130 and eDigit <= 160) or eDigit == 270:
-			return 'B5'
-		elif (eDigit >= 161 and eDigit <= 192) or eDigit in [265, 266, 267, 268]:
-			return 'B6'
-		
-
-	def getBatchReport(self, batch = 'B1'):
-		return self.batchDict.get(batch, [])
+	def getUserReport(self, enrolment):
+		# Expected enrolment as an integer
+		return self.allDict.get(enrolment.lower(), None)
